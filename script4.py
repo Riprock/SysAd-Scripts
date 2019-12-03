5a46e9c71e7e9387635466dac6afe3aafaf9d455
Evan <riprock@users.noreply.github.com>
2019-12-03 13:46:55 -0500

    Had to add some updates as It didint completely fufill the requirements so just a quick add

--- script4.py
+++ script4.py
@@ -1,18 +1,28 @@
-import re
-import geoip2.database
+import re, geoip2.database, csv, os,sys
 reader = geoip2.database.Reader("./GeoLite2-City.mmdb")
 filename = input("What log file to open?")
-pattern = re.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+if(os.path.isfile(filename)):
+    print("Reading Log")
+else:
+    print("File does not exist")
+    sys.exit(1)
+pattern = re.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")#Regex for matching valid Ip addresses
 iptonum ={}
 with open(filename) as f:
     for ln in f:
-        line = f.readline().strip().split()
+        line = f.readline().strip().split()#Setup each line for matching
         for i in line:
-            if(pattern.match(i)):
-                if(i in iptonum):
+            if(pattern.match(i)):#Check if ip addr
+                if(i in iptonum):#check if exists in dict
                     iptonum[i] += 1
                 else:
                     iptonum[i] = 1
-for key in iptonum:
-    response = reader.city(key)
-    print(str(iptonum[key]) + "," + str(key) +"," + str(response.country.name))
+with open('IPlog.csv', 'w', newline='') as file:#Create CSV file
+    writer = csv.writer(file)#create writer
+    writer.writerow(["Number of Ocurrences", "IP Address", "Country"])#header
+    for key in iptonum:
+        response = reader.city(key)
+        writer.writerow([str(iptonum[key]), str(key), str(response.country.name)])#Writes each row
+        if(iptonum[key] >= 10): # ONly display 10 or above occurnces
+            print(str(iptonum[key]) + "," + str(key) +"," + str(response.country.name))
+
